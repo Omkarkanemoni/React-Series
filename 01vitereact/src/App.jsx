e420bdca17deeb4fc6bd0860e2622b547431140d
Omkar-Kanemoni <Omkarkanemoni1029@gmail.com>
2024-10-09 18:21:24 +0530

    Vite - 2.basic react, 3.created first component,4. custom react added

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,32 @@
+
+
+import Chai from "./chai"
+
+
+function App() {
+  const username = "chai aur code"
+
+  return (
+    <>
+    <Chai/>
+    <h1>chai aur react {username}</h1>
+    <p>test para</p>
+   </>
+  )
+}
+
+export default App
+
+
+
+// import './App.css'
+
+// function App() {
+
+
+//   return (
+//     <h1>Chai aur React with Omkar</h1>
+//   )
+// }
+
+// export default App
